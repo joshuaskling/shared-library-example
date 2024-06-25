c84fb11079ba6f49de748ce1468976210a5c6f7d
Josh <joshuaskling@users.noreply.github.com>
2024-06-25 11:14:25 -0700

    Update UPDTcleaner.groovy

--- vars/UPDTcleaner.groovy
+++ vars/UPDTcleaner.groovy
@@ -5,7 +5,7 @@ def call(String input = "Running UPDTcleaner") {
             echo ${JOB_NAME}
         """
         test_var = true
-        if(${test_var} == true){
+        if("${test_var}" == true){
             echo "Passed test_var"
         } else {
             echo "Did not pass test_var"
